2ad994d00360962735450c20c24f5f2ac903fb90
EmmanuelAfolabi <ademuyiwaemmanuel07@gmail.com>
2022-04-05 05:39:51 +0100

    Second commit

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function App() {
 
       <div class="attribution">
         Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">Frontend Mentor</a>. 
-        Coded by <a href="#">Your Name Here</a>.
+        Coded by <a href="https://github.com/thenels0n/">thenelson</a>.
       </div>
     </div>
   );
